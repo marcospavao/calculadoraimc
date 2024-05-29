25d791b94c33fcfb9f28d52933c0b34444ffa711
marcos vinicius pavão <marcos81310377@gmail.com>
2024-05-29 11:24:31 -0300

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -15,21 +16,45 @@ class MainActivity : AppCompatActivity() {
         // recuperar o botao da tela
         // colocar açao no botaoc
         // recuperar o texto digitado no edt peso
+        //Sobre o float e o toString abaixo Em resumo, este código pega os valores de entrada dos campos de texto, que são strings,
+        // e os converte para valores de ponto flutuante para que possam ser utilizados em cálculos
+        // numéricos posteriores. Isso é útil em muitas aplicações, como calcular o índice de massa
+        // corporal (IMC) ou outras operações que requerem manipulação numérica precisa.
+
+
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
 
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
 
-
-
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float  = edtAltura.text.toString().toFloat()
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String  = edtAltura.text.toString()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
-            println("ação botão peso = " + resultado)
+           if (pesoStr == "" || alturaStr == ""){
+           //mostrar mensagem pra o usuario
+            Snackbar.make(
+                edtPeso,
+                    "preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+
+            )
+                .show()
+
+           } else {
+
+               val peso = pesoStr.toFloat()
+               val altura = alturaStr.toFloat()
+
+               val alturaQ2 = altura * altura
+               val resultado = peso / alturaQ2
+               println("ação botão peso = " + resultado)
+
+
+           }
+
+
         }
     }
 }
